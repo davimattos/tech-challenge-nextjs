f152137c09c62caf3bb6b8696096fa5d19922481
davimattos <davimattosdev@gmail.com>
2025-04-18 08:03:34 -0300

    chore: adjunting spaces and build proccess

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,12 @@ export const theme = {
         primary: 'rgb(var(--color-primary) / <alpha-value>)',
         'primary-hover': 'rgb(var(--color-primary-hover) / <alpha-value>)',
         'primary-soft': 'rgb(var(--color-primary-soft) / <alpha-value>)',
-
         text: {
           base: 'rgb(var(--color-text-base) / <alpha-value>)',
           muted: 'rgb(var(--color-text-muted) / <alpha-value>)',
         },
-
         background: 'rgb(var(--color-background) / <alpha-value>)',
         'background-alt': 'rgb(var(--color-background-alt) / <alpha-value>)',
-
         border: 'rgb(var(--color-border) / <alpha-value>)',
         error: 'rgb(var(--color-error) / <alpha-value>)',
         success: 'rgb(var(--color-success) / <alpha-value>)',
